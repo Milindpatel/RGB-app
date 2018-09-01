f87a2f1083e74c80d3955218a111a7a367f22761
Milindpatel <milindpatel3959@gmail.com>
2018-09-01 18:51:13 -0400

    added more functionality if we pick correct one and if we pick wrong one

--- colorgame.js
+++ colorgame.js
@@ -8,8 +8,9 @@ var colors =
 
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
@@ -21,9 +22,24 @@ squares[i].addEventListener("click", function(){
     var clickedColor = this.style.backgroundColor;
     //compare the color to picked color
     if (clickedColor === pickedColor){
-        alert("correct!");
+        messageDisplay.textContent = "Correct!";
+        changeColors(clickedColor);
     }else{
         this.style.backgroundColor = "#232323";
+        messageDisplay.textContent = "Try Again";
     }
 })
+}
+
+function changeColors(color){
+    //loop through all square
+    for(var i=0; i<color.length; i++){
+        //change each color to match given color
+        squares[i].style.backgroundColor = color;
+    }
+}
+
+function pickColor(){
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
